b14cb7988ea71a5ec88493d5bb606c6cceea563a
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-18 04:05:56 +0530

    Datatypes

---
+++ Datatypes01.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+N1=20
+N2=30
+
+Timestamp=$(date)
+
+echo "Script executed at: $Timestamp"
+
+SUM=$((N1+N2))
+
+echo "Sum of N1 and N2 is : $SUM"
+
+DIVIDE=$((N1/N2))
+
+echo "Division of N1 and N2 is : $DIVIDE"
