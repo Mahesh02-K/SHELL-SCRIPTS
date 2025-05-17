064d7345c56ed8eb9f4924d8c8614de1933219cf
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-18 03:17:13 +0530

    hello world is started

--- Convo.sh
+++ Convo.sh
@@ -3,6 +3,6 @@
 Person1=$1
 Person2=$2
 
-echo "Person1 : Hi Bruh!, How are you?"
-echo "Person2 : I'm Fine Dude!, How about you?"
-echo "Person1 : Cool!"
+echo "$Person1 : Hi Bruh!, How are you?"
+echo "$Person2 : I'm Fine Dude!, How about you?"
+echo "$Person1 : Cool!"
