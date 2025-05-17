024421252bfdf3d00a789f19b389d28fd2ad4e80
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-18 03:53:31 +0530

    Datatypes

--- Datatypes.sh
+++ Datatypes.sh
@@ -9,8 +9,8 @@ echo "Script executed at : $Timestamp"
 
 SUM=(($N1+$N2))
 
-echo "Sum of $N1 and $N2 is $SUM"
+echo "Sum of $N1 and $N2 is: $SUM"
 
 MULTIPLY=(($N1+$N2))
 
-echo "Multiply of $N1 and $N2 is $MULTIPLY"
+echo "Multiply of $N1 and $N2 is: $MULTIPLY"
