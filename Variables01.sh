46123ba4de5948b67473b333b221ba7e847afdc2
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-18 03:28:42 +0530

    variables

---
+++ Variables01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1 : Hi $Person2! How are you doing? "
+echo "$Person2 : Hi $Person1! Im doing good!, How about you?"
+echo "$Person1 : cool!, what's up?"
+echo "$Person2 : Nothing!"
