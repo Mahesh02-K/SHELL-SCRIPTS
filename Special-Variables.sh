b3a96779017bdb88af766ed40aef44fd1da7035e
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-18 19:22:11 +0530

    special vars in shell

---
+++ Special-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "number of variables passed to script : $#"
+echo "all variables passed to script : $@"
+echo "Current directory : $PWD"
+echo "home directory of user : $HOME"
+echo "PID of the script : $$"
+echo "PID of last command running in background : $!"
+echo "Script name : $?"
