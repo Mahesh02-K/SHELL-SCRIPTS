686d3ea8da7d7d0ad8a021e3818bc56f3b0e0d02
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-19 12:58:41 +0530

    Conditions

--- conditions1.sh
+++ conditions1.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-if [ umbrella ]
-then 
-    echo "don't go"
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : PLEASE RUN WITH ROOT ACCESS"
+    exit 1
 else
-    echo "go"
+    echo "You are running with ROOT ACCESS"
+fi
+dnf list installed MySQL
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed.. going to install it"
+    dnf install MySQL -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing MySQL is SUCCESS..."
+    else 
+        echo "Installing MySQL is FAILURE..."
+        exit 1
+    fi
+else
+    echo "MySQL is already installed.. Nothing to do"
 fi
