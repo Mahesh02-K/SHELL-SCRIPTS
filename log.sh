7b9c583fef77ef043e9ed2f939f9937c1d13e381
Mahesh02-K <kakuturumahesh@gmail.com>
2025-05-20 16:44:36 +0530

    LOGS with colors

--- log.sh
+++ log.sh
@@ -7,51 +7,61 @@ G="\e[32m"
 M="\e[35m"
 B="\e[34m"
 N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at : $(date)" | tee -a $LOG_FILE
 
 if [ "$USERID" -ne 0 ]
 then
-    echo -e " $G ERROR:You are not running with root access $N "
+    echo -e " $G ERROR:You are not running with root access $N " | tee -a $LOG_FILE
     exit 1
 else
-    echo -e " $R You are running with ROOT access $N "
+    echo -e " $R You are running with ROOT access $N " | tee -a $LOG_FILE
 fi
 
 VERIFY(){
     if [ $1 -eq 0 ]
     then 
-        echo -e "Installing $2 is .. $R SUCCESS $N "
+        echo -e "Installing $2 is .. $R SUCCESS $N " | tee -a $LOG_FILE
     else
-        echo -e "Installing $2 is .. $G FAILURE $N "
+        echo -e "Installing $2 is .. $G FAILURE $N " | tee -a $LOG_FILE
     fi
 }
 # VERIFY is a function that takes input as exit status and what command they try to install
 
 
-dnf list installed mysql
+dnf list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo -e " $M mysql is not installed.. going to install it $N "
-    dnf install mysql -y
+    echo -e " $M mysql is not installed.. going to install it $N " | tee -a $LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE
     VERIFY $? mysql
 else
-    echo -e " $B mysql is already installed.. nothing to do it $N "
+    echo -e " $B mysql is already installed.. nothing to do it $N " | tee -a $LOG_FILE
 fi 
 
-dnf list installed python3
+dnf list installed python3 &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo -e " $M python3 is not installed.. going to install it $N "
-    dnf install python3 -y
+    echo -e " $M python3 is not installed.. going to install it $N " | tee -a $LOG_FILE
+    dnf install python3 -y &>>$LOG_FILE
     VERIFY $? python3
 else
-    echo -e " $B python3 is already installed.. nothing to do it $N "
+    echo -e " $B python3 is already installed.. nothing to do it $N " | tee -a $LOG_FILE
 fi
-dnf list installed nginx
+dnf list installed nginx &>>LOG_FILE
 if [ $? -ne 0 ]
 then 
-    echo -e " $M nginx is not installed.. going to install it $N "
-    dnf install nginx -y
+    echo -e " $M nginx is not installed.. going to install it $N " | tee -a $LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
     VERIFY $? nginx
 else 
-    echo -e " $B nginx is already installed.. nothing to do it $N "
+    echo -e " $B nginx is already installed.. nothing to do it $N " | tee -a $LOG_FILE
 fi
+ 
+#  | tee -a $LOG_FILE this command will print the output onscreen and upload into logfile 
+#  &>>$LOG_FILE this command will redirect the output into logfile
+#  -e is used for enable the colors
